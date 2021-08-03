b98bd165709b903893c5815ba5c89c88308da29f
Kirito <kurulluvarov@gmail.com>
2021-08-03 14:54:42 +0300

    requests

--- src/lessons/lesson3/lesson_3.ts
+++ src/lessons/lesson3/lesson_3.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 console.log('lesson 3');
 
 // Event loop
@@ -14,16 +16,11 @@ console.log('lesson 3');
 
 // https://jsonplaceholder.typicode.com/posts/1
 // https://habr.com/ru/company/oleg-bunin/blog/417461/?_ga=2.54695343.543933152.1602500664-1040035071.1596811661
-async function f() {
-try{
-     throw new Error
-}
-catch(e){
-    console.log(e);
-    
-}
-}
-console.log(f());
+axios.get('https://jsonplaceholder.typicode.com/posts/1').then(response=>console.log(response.data))
+axios.post('https://jsonplaceholder.typicode.com/posts',{title:'KirKo'}).then(response=>console.log(response.data))
+axios.put('https://jsonplaceholder.typicode.com/posts/1',{title:'Kiruha'}).then(response=>console.log(response.data))
+axios.delete('https://jsonplaceholder.typicode.com/posts/1')
+axios.get('https://jsonplaceholder.typicode.com/posts').then(response=>console.log(response.data))
 
 // just a plug
 export default ()=>{};
